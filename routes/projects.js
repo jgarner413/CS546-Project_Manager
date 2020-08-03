69560ccb386b26740fce28c9d02f9c1a1943532c
jgarner413 <garner@Joshs-MBP.fios-router.home>
2020-08-03 19:34:20 -0400

    added requirements to routes project.js

---
+++ routes/projects.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const projectData = data.projects;
+const userData = data.users;
+const { ObjectId } = require('mongodb');
